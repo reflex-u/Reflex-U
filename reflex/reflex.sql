dd485d7635a1995f2832de0436808b1546ac3070
amyliaahamad <amyliaahamad@192.168.88.1>
2018-01-01 02:53:48 +0800

    new new new

--- reflex/reflex.sql
+++ reflex/reflex.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `booking`
 --
 
+create database if not exists reflex;
+use reflex; 
+
 DROP TABLE IF EXISTS `booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
